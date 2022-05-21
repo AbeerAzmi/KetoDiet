a12c128512e29ea4459ec630ea8daf9c147356a4
Abeer Azmi <abeeerazmi@gmail.com>
2022-05-21 21:50:37 +0400

    Updated title

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Abeer',
+      title: 'Flutter Bero',
       theme: ThemeData(
         primarySwatch: Colors.blue,
         textTheme: TextTheme(
